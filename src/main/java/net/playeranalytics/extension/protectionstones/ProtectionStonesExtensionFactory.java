4fb58e311b070bf736838576c0cc625f9271d4d5
Aurora Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2022-07-13 11:50:46 +0300

    R1.2: Package change, junit scope

--- src/main/java/net/playeranalytics/extension/protectionstones/ProtectionStonesExtensionFactory.java
+++ src/main/java/net/playeranalytics/extension/protectionstones/ProtectionStonesExtensionFactory.java
@@ -20,33 +20,32 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
     THE SOFTWARE.
 */
-package com.djrapitops.extension;
+package net.playeranalytics.extension.protectionstones;
 
 import com.djrapitops.plan.extension.DataExtension;
-import com.djrapitops.plan.extension.extractor.ExtensionExtractor;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 /**
- * Test for the implementation of the new extension
+ * Factory for DataExtension.
  *
  * @author AuroraLS3
  */
-class ExtensionImplementationTest {
+public class ProtectionStonesExtensionFactory {
 
-    private ExtensionExtractor extractor;
-
-    @BeforeEach
-    void prepareExtractor() {
-        DataExtension extension = new ProtectionStonesExtension();
-        extractor = new ExtensionExtractor(extension);
+    private boolean isAvailable() {
+        try {
+            Class.forName("dev.espi.protectionstones.PSPlayer");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
     }
 
-    @Test
-    @DisplayName("API is implemented correctly")
-    void noImplementationErrors() {
-        extractor.validateAnnotations();
+    public Optional<DataExtension> createExtension() {
+        if (isAvailable()) {
+            return Optional.of(new ProtectionStonesExtension());
+        }
+        return Optional.empty();
     }
-
 }
